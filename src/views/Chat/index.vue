f33c9f1fe804aa44f0790a5461f2d8c612fe5ea0
948967007 <50066580+948967007@users.noreply.github.com>
2019-09-21 17:12:47 +0800

    pdd
    pdd

---
+++ src/views/Chat/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="Chat">
+        聊天
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Chat"
+    }
+</script>
+
+<style scoped>
+    .Chat {
+        height: 100vh;
+        background-color: hotpink;
+    }
+</style>
